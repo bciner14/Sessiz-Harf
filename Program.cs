7c7aa2e76a927aafe1259e1bdbc0410e0c1739ba
bciner14 <143399567+bciner14@users.noreply.github.com>
2024-07-17 12:21:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the string: ");
+        string input = Console.ReadLine();
+        
+        string[] words = input.Split(' ');
+
+        foreach (string word in words)
+        {
+            bool hasConsecutiveConsonants = HasConsecutiveConsonants(word);
+            Console.WriteLine(hasConsecutiveConsonants);
+        }
+    }
+
+    static bool HasConsecutiveConsonants(string word)
+    {
+        HashSet<char> vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+        
+        for (int i = 0; i < word.Length - 1; i++)
+        {
+            if (!vowels.Contains(word[i]) && !vowels.Contains(word[i + 1]))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
